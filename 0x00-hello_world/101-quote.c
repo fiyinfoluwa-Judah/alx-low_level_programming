6f9791310a3abc3fe88c3538556ac946dc95fccb
fiyinfoluwa adeite <fiyinfoluwaadeite57@gmail.com>
2023-03-18 06:35:42 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-gcc -S .masm-intel $CFILE
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A c program that prints a line to the strandard error
+ * Return 1 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is usefu\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}

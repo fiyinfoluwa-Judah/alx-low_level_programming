e48a4627da64612969320e61343e4e520b439ffd
fiyinfoluwa adeite <fiyinfoluwaadeite57@gmail.com>
2023-03-22 05:38:29 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - tests function that prints integer is postive or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	i = 0;
+	positive or negative(i);
+	return (0);
+}	
